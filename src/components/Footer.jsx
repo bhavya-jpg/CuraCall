b14aa6ff1427c1b74fea64c268a8ff517a6f6115
bhavya-jpg <bhavya120906@gmail.com>
2025-08-18 17:17:20 +0530

    banner section complete and footer started

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            {/*--------left section-------*/}
+            <div>
+                 img
+            </div>
+
+            {/*--------center section--------*/}
+            <div></div>
+
+            {/*-------right section------*/}
+            <div></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
