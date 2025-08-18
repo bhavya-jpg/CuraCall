e5ab9ccb9df64b288671677b7b656ee53e080888
bhavya-jpg <bhavya120906@gmail.com>
2025-08-18 17:50:48 +0530

    home page ui complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="md:md-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm">
+        {/*--------left section-------*/}
         <div>
-            {/*--------left section-------*/}
-            <div>
-                 img
-            </div>
-
-            {/*--------center section--------*/}
-            <div></div>
-
-            {/*-------right section------*/}
-            <div></div>
-
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        {/*--------center section--------*/}
+        <div className="text-xl font-medium mb-5">
+          <p>COMPANY</p>
+          <ul className="flex flex-col text-gray-600 gap-2">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        {/*-------right section------*/}
+        <div className="text-xl font-medium mb-5">
+          <p>GET IN TOUCH</p>
+          <ul className="flex flex-col text-gray-600 gap-2">
+            <li>+1-212-456-7890</li>
+            <li>bhavya120906@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      {/*--------Copyright Text--------*/}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">Copyright © 2024 PRESCRIPTO- All Right Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
